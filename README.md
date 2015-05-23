d554d02f91b57a293ca2726459a82e43a5147ff2
Nico Di Rocco <n.dirocco@tech.leaseweb.com>
2015-05-23 09:47:37 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,7 @@
+TODO: git hook for detecting composer changes
+
+TODO: git hook for detecting doctrine migrations
+
+TODO: store paths to ignore for ctrlp-warmup-cache in repository .git/config
+
+TODO: default git commit template that contains INFRA- on the first line
---
+++ ctags-warmup-cache.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -e
+
+CTAGS_CONFIG='/home/nrocco/.dotfiles/ctags/ctags-php'
+GIT_DIR=`git rev-parse --git-dir`
+
+if [[ '--vendors' == "$1" ]]
+then
+
+    time (
+        find vendor -type f -name '*.php' | \
+            /usr/bin/ctags -L - --tag-relative --options=${CTAGS_CONFIG} -o ${GIT_DIR}/tags_vendors
+    )
+
+else
+
+    time (
+        git ls-files | /bin/grep -E 'php$' | \
+            /usr/bin/ctags -L - --tag-relative --options=${CTAGS_CONFIG} -o ${GIT_DIR}/tags
+    )
+
+fi
