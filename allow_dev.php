18c53f8e3ee2e29510409e8c6a1178a43d46422a
Nico Di Rocco <n.dirocco@tech.leaseweb.com>
2015-05-26 20:48:30 +0200

    remove script that does not belong in this repo

--- allow_dev.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-// This is a hack to avoid having local modifications in all app_dev.php front controllers
-if ("/app_dev.php" === $_SERVER['SCRIPT_NAME']) {
-    $_SERVER['REMOTE_ADDR'] = '127.0.0.1';
-}
