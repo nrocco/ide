d29401fb3c8f0caaeef31dd7b3f563446e1230f3
Nico Di Rocco <dirocco.nico@gmail.com>
2017-07-20 22:43:09 +0200

    Allow version sub command to be run anywhere

--- cmd/version.go
+++ cmd/version.go
@@ -10,6 +10,8 @@ var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Get the version of ide",
 	Long:  ``,
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+	},
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Printf("ide: %s\n", Version)
 	},
