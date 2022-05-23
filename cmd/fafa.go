55239453e6aea1bf23c98d13d1b0f652ae67bf1a
Nico Di Rocco <dirocco.nico@gmail.com>
2022-05-23 20:13:31 +0200

    Add hidden command for testing things

---
+++ cmd/fafa.go
@@ -0,0 +1,19 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+var fafaCmd = &cobra.Command{
+	Use:     "fafa",
+	Hidden:  true,
+	PreRunE: loadProject,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		// return project.AddGitBinToPath()
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(fafaCmd)
+}
